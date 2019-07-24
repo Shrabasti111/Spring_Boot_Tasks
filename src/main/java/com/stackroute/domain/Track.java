5b1cc7e588c25fbb080ee45d065e8de672766523
shrabro <shrabro1@in.ibm.com>
2019-07-24 17:30:05 +0530

    ninth task

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Track {
 
     @Id
@@ -15,45 +18,4 @@ public class Track {
     private String name;
     private String comments;
 
-    public Track() {
-    }
-
-    public Track(int id, String name, String comments) {
-        this.id = id;
-        this.name = name;
-        this.comments = comments;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
 }
