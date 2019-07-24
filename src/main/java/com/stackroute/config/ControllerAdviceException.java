e20ee2b8cc73152c20a29a5dd8900401bd6eb506
shrabro <shrabro1@in.ibm.com>
2019-07-24 16:53:40 +0530

    seventh task

---
+++ src/main/java/com/stackroute/config/ControllerAdviceException.java
@@ -0,0 +1,16 @@
+package com.stackroute.config;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdviceException {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception e) {
+        return new ResponseEntity<>("Error thrown globally : " + e.getMessage(), HttpStatus.CONFLICT);
+    }
+}
