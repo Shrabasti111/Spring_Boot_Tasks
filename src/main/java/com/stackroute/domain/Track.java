a42191436f46eabb4e8cc5051d3da98399cfc8fd
Shrabasti111 <51950071+Shrabasti111@users.noreply.github.com>
2019-08-07 14:36:15 +0530

    Update Track.java

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,9 +1,4 @@
 package com.stackroute.domain;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
