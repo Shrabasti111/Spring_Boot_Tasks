88e8f77dc47dd315d75ce1853d29d19473735b8c
Shrabasti111 <51950071+Shrabasti111@users.noreply.github.com>
2019-08-07 19:42:06 +0530

    Update MusicServiceImpl.java

--- src/main/java/com/stackroute/service/MusicServiceImpl.java
+++ src/main/java/com/stackroute/service/MusicServiceImpl.java
@@ -1,6 +1,5 @@
 package com.stackroute.service;
 
-
 import com.stackroute.domain.Track;
 import com.stackroute.exceptions.TrackAlreadyExistsException;
 import com.stackroute.exceptions.TrackNotFoundException;
@@ -18,7 +17,7 @@ import java.util.Optional;
 @Service
 public class MusicServiceImpl implements MusicService, ApplicationListener<ContextRefreshedEvent>, CommandLineRunner {
 
-    MusicRepository musicRepository;
+    private MusicRepository musicRepository;
 
     @Autowired
     public MusicServiceImpl(MusicRepository musicRepository) {
@@ -39,10 +38,6 @@ public class MusicServiceImpl implements MusicService, ApplicationListener<Conte
         return savedTrack;
     }
 
-    @Override
-    public List<Track> getTrack() {
-        return (List<Track>)musicRepository.findAll();
-    }
 
     @Override
     public Track getById(int id) throws TrackNotFoundException {
@@ -56,9 +51,9 @@ public class MusicServiceImpl implements MusicService, ApplicationListener<Conte
 
         return getByIdTrack.get();
     }
-
+    
     @Override
-    public void deleteById(int id) throws TrackNotFoundException {
+    public List<Track> deleteById(int id) throws TrackNotFoundException {
 
         Optional<Track> getByIdTrack = musicRepository.findById(id);
 
@@ -66,11 +61,13 @@ public class MusicServiceImpl implements MusicService, ApplicationListener<Conte
             throw new TrackNotFoundException("Track does not exist");
         }
         musicRepository.deleteById(id);
+        List<Track> trackList = musicRepository.findAll();
+        return trackList;
 
     }
 
     @Override
-    public boolean updateById(Track track, int id) throws TrackNotFoundException {
+    public Track updateById(Track track, int id) throws TrackNotFoundException {
 
         Optional<Track> updateTrack = musicRepository.findById(id);
 
@@ -81,13 +78,26 @@ public class MusicServiceImpl implements MusicService, ApplicationListener<Conte
 
         track.setId(id);
         musicRepository.save(track);
-        return true;
+        return updateTrack.get();
     }
 
     @Override
-    public List<Track> getTrackByName(String name) {
+    public List<Track> getTrack() throws TrackNotFoundException {
+        List<Track> trackList = musicRepository.findAll();
+        if(trackList.isEmpty()) {
+            throw new TrackNotFoundException("No tracks found");
+        }
+        return trackList;
+    }
+    
+   @Override
+    public List<Track> getTrackByName(String name) throws TrackNotFoundException {
 
         List<Track> user_id = musicRepository.getTrackByName(name);
+
+        if(user_id.isEmpty()) {
+            throw new TrackNotFoundException("Track not found");
+        }
 
         return user_id;
     }
