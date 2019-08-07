2156fc6591f181285988059e200b330e604138ac
Shrabasti111 <51950071+Shrabasti111@users.noreply.github.com>
2019-08-07 19:09:47 +0530

    Update MusicServiceImpl.java

--- src/main/java/com/stackroute/service/MusicServiceImpl.java
+++ src/main/java/com/stackroute/service/MusicServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 public class MusicServiceImpl implements MusicService {
 
-    MusicRepository musicRepository;
+    private MusicRepository musicRepository;
 
     @Autowired
     public MusicServiceImpl(MusicRepository musicRepository) {
